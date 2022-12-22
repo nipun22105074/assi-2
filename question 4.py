c222eb3ef17fe0445c916f31a408a4d4c3ebb4d6
nipun22105074 <121248279+nipun22105074@users.noreply.github.com>
2022-12-22 22:34:30 +0530

    Add files via upload

---
+++ question 4.py
@@ -0,0 +1,10 @@
+a=int(input(""))
+b=int(input("enter number 2: "))
+c=int(input("enter number 3: "))
+if(a>b and a>c):
+    print("the greatest number is" ,a)
+elif(b>c and b>a):
+    print("the greatest number is" ,b)
+else:
+    print("the greatest number is", c)
+    
